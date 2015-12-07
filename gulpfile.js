f04b3fb935b4075a3cf95bf535adcad45a1837a0
Talento90 <marcotalento90@gmail.com>
2015-12-07 22:52:16 +0000

    added clean and source maps

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,41 @@ var gulp    = require('gulp');
 var tslint  = require('gulp-tslint');
 var mocha = require('gulp-mocha');
 var gulp    = require('gulp-help')(gulp);
+var clean = require('gulp-clean');
+var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
 var nodemon = require('gulp-nodemon');
+
 
 var tsconfig = require('./tsconfig');
 var sourceFiles = 'src/**/*.ts';
 var testFiles = 'test/**/*.ts';
-var tsFiles = [sourceFiles, testFiles];
+var srcOption = { base: './' };
 var tsProject = ts.createProject('tsconfig.json', {
     typescript: require('typescript')
 });
-
+var outDir = tsconfig.compilerOptions.outDir;
+var tsFiles = [sourceFiles, testFiles];
 
 gulp.task('nodemon', 'Run nodemon (Build and Watch Ts files)', ['build', 'watch'], function() {  
     nodemon({
         script: './build/src/app.js',
-        //exec: 'babel-node'
     });
 });
 
-gulp.task('build', 'Build all Typescript files.', function () {
- var outDir = tsconfig.compilerOptions.outDir;
- 
- var tsResult = gulp.src(tsFiles)
+gulp.task('clean', 'Clean build folder.', function () {
+    return gulp.src(outDir, {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', 'Build all Typescript files.', function () { 
+ var tsResult = gulp.src(tsFiles, srcOption)
+        .pipe(sourcemaps.init())
         .pipe(ts(tsProject));
-        return tsResult.js.pipe(gulp.dest(outDir));
+        
+    return tsResult.js
+         .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../..' }))
+        .pipe(gulp.dest(outDir));
 });
 
 gulp.task('watch', 'Watch all ts files.', function() {  
