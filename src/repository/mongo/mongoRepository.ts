4bbb5ab1766366049eb1f49eb08e8a7829554a50
Marco Talento <marco.r.talento@parceiros.nos.pt>
2016-02-24 14:03:16 +0000

    working on repository

--- src/repository/mongo/mongoRepository.ts
+++ src/repository/mongo/mongoRepository.ts
@@ -1,6 +1,16 @@
 import { IRepository, IEntity } from '../interfaces';
+import * as Mongoose from 'mongoose';
+
+
+new Mongoose.Schema()
 
 export default class MongoRepository<T extends IEntity> implements IRepository<T> {
+    
+    protected _model: Mongoose.Model<Mongoose.Document>;
+    
+    constructor() {
+        //_model =   
+    }
 
     public getAll(): Promise<Array<T>> {
         return Promise.resolve();
