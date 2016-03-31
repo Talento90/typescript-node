ce8d01f1d3b5f167e8c42946c904fec98ec43847
Marco Talento <marco.r.talento@parceiros.nos.pt>
2016-03-31 13:18:37 +0100

    working on tsc tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,10 @@
 
 let gulp = require('gulp');
 let rimraf = require('gulp-rimraf');
-let tsc = require('gulp-typescript');
-let sourcemaps = require('gulp-sourcemaps');
 let tslint = require('gulp-tslint');
 let mocha = require('gulp-mocha');
 let path = require('path');
+let exec = require('child_process').exec;
 
 // /*  Variables */
 let tsProject = tsc.createProject('tsconfig.json');
@@ -35,21 +34,41 @@ gulp.task('tslint', () => {
 /**
  * Compile TypeScript sources and create sourcemaps in build directory.
  */
-gulp.task('compile', ['clean'], () => {
-  let tsResult = gulp.src([sourceFiles, testFiles])
-    .pipe(sourcemaps.init())
-    .pipe(tsc(tsProject));
-  return tsResult.js
-    .pipe(sourcemaps.write('.', { sourceRoot: sourceRoot }))
-    .pipe(gulp.dest(outDir));
+
+gulp.task('compile', (cb) => {
+  exec('tsc --version', (err, stdout, stderr) => {
+    console.log('TypeScript ', stdout);
+    if (stderr) {
+      console.log(stderr);
+    }
+  });
+
+  return exec('tsc', (err, stdout, stderr) => {
+    console.log(stdout);
+    if (stderr) {
+      console.log(stderr);
+    }
+    cb(err);
+  });
 });
 
 /**
  * Watch for changes in TypeScript
  */
-gulp.task('watch', function () {
-  gulp.watch([sourceFiles, testFiles], ['compile']).on('change', function (e) {
-    console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
+gulp.task('watch', (cb) => {
+  exec('tsc --version', (err, stdout, stderr) => {
+    console.log('TypeScript ', stdout);
+    if (stderr) {
+      console.log(stderr);
+    }
+  });
+
+  return exec('tsc -w', (err, stdout, stderr) => {
+    console.log(stdout);
+    if (stderr) {
+      console.log(stderr);
+    }
+    cb(err);
   });
 });
 
