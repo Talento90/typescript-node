c96cb5ebe0b82be6630ea27a149653a26253e040
Talento90 <marcotalento90@gmail.com>
2016-05-29 21:39:31 +0100

    add missing package

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="typings\main.d.ts" />
-/// <reference path="node_modules/inversify/type_definitions/inversify/inversify.d.ts" />
+/// <reference path="node_modules/inversify-dts/inversify/inversify.d.ts" />
 
 declare module "node-uuid" {
     export function v4();
